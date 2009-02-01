dbbbdf244e857de0587603ed1ee106aa030d8a9d
misko.hevery <misko.hevery@639a558c-a384-11dd-a8c6-677c70d10fe4>
2009-02-01 22:44:49 +0000

    Fix: http://code.google.com/p/classpath-explorer/issues/detail?id=5

--- src-test/com/google/classpath/ClassPathTest.java
+++ src-test/com/google/classpath/ClassPathTest.java
@@ -16,89 +16,101 @@
 package com.google.classpath;
 
 import static com.google.classpath.RegExpResourceFilter.ANY;
-import static java.util.Arrays.asList;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import junit.framework.TestCase;
 
 public abstract class ClassPathTest extends TestCase {
 
-	protected ClassPath path;
+  protected ClassPath path;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		path = createClassPath();
-	}
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    path = createClassPath();
+  }
 
-	abstract protected ClassPath createClassPath() throws IOException;
+  abstract protected ClassPath createClassPath() throws IOException;
 
-	private String read(InputStream is) throws IOException {
-		StringBuilder buf = new StringBuilder();
-		int ch;
-		while ((ch = is.read()) != -1) {
-			buf.append((char) ch);
-		}
-		is.close();
-		return buf.toString();
-	}
+  private String read(InputStream is) throws IOException {
+    StringBuilder buf = new StringBuilder();
+    int ch;
+    while ((ch = is.read()) != -1) {
+      buf.append((char) ch);
+    }
+    is.close();
+    return buf.toString();
+  }
 
-	public void assertArrayEquals(String[] actual, String... expected) {
-		String error = String.format("Expected: '%s' Actual: '%s'", Arrays
-				.toString(expected), Arrays.toString(actual));
-		assertEquals(error, asList(actual), asList(expected));
-	}
+  public void assertArrayEqualsAnyOrder(String[] actual, String... expected) {
+    Set<String> expectedAsSet = new HashSet<String>(Arrays.asList(expected));
+    assertEquals(
+        "Expected not allowed to contain duplicates for this assertion, but does.",
+        expectedAsSet.size(), expected.length);
 
-	public void testCheckForResource() throws Exception {
-		assertTrue(path.isResource("A/1.file"));
-		assertTrue(path.isResource("/A/1.file"));
-		assertFalse(path.isResource("/A/1.file/"));
-		assertTrue(path.isResource("/A/2.file"));
-		assertFalse(path.isResource("NON_EXISTANT"));
-		assertFalse(path.isResource("A"));
-		assertFalse(path.isResource("/A"));
-		assertFalse(path.isResource("/A/"));
-		assertFalse(path.isPackage("NON_EXISTANT"));
-	}
+    String error = String.format(
+        "Expected (%s length) not same as Actual (%s length)", expected.length,
+        actual.length);
+    assertEquals(error, expected.length, actual.length);
 
-	public void testCheckForPackage() throws Exception {
-		assertTrue(path.isPackage("A"));
-		assertFalse(path.isResource("A"));
-		assertFalse(path.isPackage("NON_EXISTANT"));
-	}
+    Set<String> actualAsSet = new HashSet<String>(Arrays.asList(actual));
+    error = String.format("Expected: '%s' Actual: '%s'", expected, actualAsSet);
+    assertEquals(error, actualAsSet, expectedAsSet);
+  }
 
-	public void testListPackages() throws Exception {
-		assertArrayEquals(path.listPackages(""), "A", "META-INF");
-		assertArrayEquals(path.listPackages("/"), "A", "META-INF");
-		assertArrayEquals(path.listPackages("A"), "B", "C");
-		assertArrayEquals(path.listPackages("/A"), "B", "C");
-		assertArrayEquals(path.listPackages("A/"), "B", "C");
-		assertArrayEquals(path.listPackages("/A/"), "B", "C");
-	}
+  public void testCheckForResource() throws Exception {
+    assertTrue(path.isResource("A/1.file"));
+    assertTrue(path.isResource("/A/1.file"));
+    assertFalse(path.isResource("/A/1.file/"));
+    assertTrue(path.isResource("/A/2.file"));
+    assertFalse(path.isResource("NON_EXISTANT"));
+    assertFalse(path.isResource("A"));
+    assertFalse(path.isResource("/A"));
+    assertFalse(path.isResource("/A/"));
+    assertFalse(path.isPackage("NON_EXISTANT"));
+  }
 
-	public void testListResources() throws Exception {
-		assertArrayEquals(path.listResources(""));
-		assertArrayEquals(path.listResources("/"));
-		assertArrayEquals(path.listResources("A"), "1.file", "2.file");
-		assertArrayEquals(path.listResources("/A"), "1.file", "2.file");
-		assertArrayEquals(path.listResources("/A/"), "1.file", "2.file");
-	}
+  public void testCheckForPackage() throws Exception {
+    assertTrue(path.isPackage("A"));
+    assertFalse(path.isResource("A"));
+    assertFalse(path.isPackage("NON_EXISTANT"));
+  }
 
-	public void testFindResources() throws Exception {
+  public void testListPackages() throws Exception {
+    assertArrayEqualsAnyOrder(path.listPackages(""), "A", "META-INF");
+    assertArrayEqualsAnyOrder(path.listPackages("/"), "A", "META-INF");
+    assertArrayEqualsAnyOrder(path.listPackages("A"), "B", "C");
+    assertArrayEqualsAnyOrder(path.listPackages("/A"), "B", "C");
+    assertArrayEqualsAnyOrder(path.listPackages("A/"), "B", "C");
+    assertArrayEqualsAnyOrder(path.listPackages("/A/"), "B", "C");
+  }
+
+  public void testListResources() throws Exception {
+    assertArrayEqualsAnyOrder(path.listResources(""));
+    assertArrayEqualsAnyOrder(path.listResources("/"));
+    assertArrayEqualsAnyOrder(path.listResources("A"), "1.file", "2.file");
+    assertArrayEqualsAnyOrder(path.listResources("/A"), "1.file", "2.file");
+    assertArrayEqualsAnyOrder(path.listResources("/A/"), "1.file", "2.file");
+  }
+
+  public void testFindResources() throws Exception {
     RegExpResourceFilter anyFile = new RegExpResourceFilter(ANY, ".*\\.file");
-    assertArrayEquals(path.findResources("X", anyFile));
-    assertArrayEquals(path.findResources("", anyFile), "A/1.file", "A/2.file", "A/B/3.file");
-	}
+    assertArrayEqualsAnyOrder(path.findResources("X", anyFile));
+    assertArrayEqualsAnyOrder(path.findResources("", anyFile), "A/1.file",
+        "A/2.file", "A/B/3.file");
+  }
 
-	public void testReadResource() throws Exception {
-		assertEquals("FILE1", read(path.getResourceAsStream("A/1.file")));
-		assertEquals("FILE1", read(path.getResourceAsStream("/A/1.file")));
-		assertNull(path.getResourceAsStream("A/1.file/"));
-		assertNull(path.getResourceAsStream("/A/1.file/"));
-		assertNull(path.getResourceAsStream("NON_EXISTANT"));
-	}
+  public void testReadResource() throws Exception {
+    assertEquals("FILE1", read(path.getResourceAsStream("A/1.file")));
+    assertEquals("FILE1", read(path.getResourceAsStream("/A/1.file")));
+    assertNull(path.getResourceAsStream("A/1.file/"));
+    assertNull(path.getResourceAsStream("/A/1.file/"));
+    assertNull(path.getResourceAsStream("NON_EXISTANT"));
+  }
 
 }
